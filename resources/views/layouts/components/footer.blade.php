73160ed06d76bda52904ec09713ed22846fc17df
JONVILLE Guillaume <g.jonville@b2ei.com>
2018-03-25 11:38:50 +0200

    Add update version

--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 0.7.5
+        <b>Version</b> {{config('app.version')}}
     </div>
     <strong>Copyright &copy; 2018-2020 <a href="http://www.b2ei.com">B2EI</a>.</strong> All rights
     reserved.
